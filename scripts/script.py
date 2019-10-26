5650d267ad69e5f706ff61dc089ab01c3836a0ed
Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>
2019-10-26 16:15:02 +0530

    udpated the script, added dummy data
    added dummy data

--- scripts/script.py
+++ scripts/script.py
@@ -3,51 +3,69 @@ import json
 import collections
 import re 
 
-accessToken = "<--insert your accessToken here-->"
+from secret import accessToken
+
 headers = {"Authorization": "bearer "+ accessToken }
 
-def getpullRequests(username):
-    topic_query = """
-    query ($name:String!){
-    repositoryOwner(login:$name){
-        login 
-        ... on User {
-        name
-        avatarUrl
-        pullRequests(last: 100){
-            nodes{
-            id
-            createdAt
-            additions
-            deletions
+topic_query = """
+        query ($name:String!){
+        repositoryOwner(login:$name){
+            login 
+            ... on User {
+            name
+            avatarUrl
+            pullRequests(last: 100){
+                nodes{
+                id
+                createdAt
+                additions
+                deletions
+                }
             }
+            } 
         }
         } 
-    }
-    } 
-    """
-    variables=dict({
-        "name":str(username)
-    })
-    request = requests.post('https://api.github.com/graphql', json={'query': topic_query,"variables":variables},headers=headers)
-    if request.status_code == 200:
-        result = request.json()
-        prsdata = {}
-        for pr in result['data']['repositoryOwner']['pullRequests']['nodes']:
-            prdata = {}
-            if re.match(r'^2019-10', pr['createdAt']):
-                prdata['createdAt'] = pr['createdAt']
-                prdata['additions'] = pr['additions']
-                prdata['deletions'] = pr['deletions']
-                prsdata[pr['id']] = prdata
-        if prsdata:
-            print(prsdata)
+        """
+
+class pullRequestsData:
+
+    def __init__(self, username):
+        self.variables=dict({
+            "name":str(username)
+        })
+        self.data = {}
+
+    def getPRData(self):
+        request = requests.post('https://api.github.com/graphql', json={'query': topic_query,"variables":self.variables},headers=headers)
+        if(request.status_code == 200):
+            result = request.json()           
+            if(result['data']['repositoryOwner'] != None):
+                self.data['username'] = result['data']['repositoryOwner']['login']
+                self.data['name'] = result['data']['repositoryOwner']['name']
+                prsdata = {}
+                for pr in result['data']['repositoryOwner']['pullRequests']['nodes']:
+                    prdata = {}
+                    if(re.match(r'^2019-10', pr['createdAt'])):
+                        prdata['createdAt'] = pr['createdAt']
+                        prdata['additions'] = pr['additions']
+                        prdata['deletions'] = pr['deletions']
+                        prsdata[pr['id']] = prdata
+                self.data['prsdata'] = prsdata
+            else:
+                self.data['username'] = result['data']['repositoryOwner']
+
+    def displayData(self):
+        if(self.data['username'] != None):
+            if(self.data['prsdata']):
+                print(self.data)
+            else:
+                print("No PRs for Hacktoberfest")
         else:
-            print("No PRs made in Hacktoberfest")
+            print("User doesn't exist")
+
 
 if __name__ == "__main__":
-    try:
-        username = input("Enter the GitHub username: ")
-        getpullRequests(username)
-    except TypeError:
-        print("No user exists")
+    username = input("Enter the GitHub username: ")
+    prData = pullRequestsData(username)
+    prData.getPRData()
+    prData.displayData()
